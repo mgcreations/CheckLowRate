292c11e80f402366817496f8411057bf0b9f8de2
MG Creations <info@mgcreations.co.za>
2020-06-21 19:57:22 +0200

    Minor improves

--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ namespace CheckLowRate
         static void Main()
         {
             double hourlyRate;
+            const double LOW_RATE = 5.65;
 
             Console.Write("Enter the hourly rate of pay: ");
             hourlyRate = Convert.ToDouble(Console.ReadLine());
 
-            if (hourlyRate <= 5.65)
-                Console.WriteLine("Error: Hourly Rate too low.");
+            if (hourlyRate <= LOW_RATE)
+                Console.WriteLine("Error: You entered an amount lower than {0}", LOW_RATE.ToString("C"));
+            else
+                Console.WriteLine("You entered {0} as the hourly rate.", hourlyRate.ToString("C"));
         }
     }
 }
