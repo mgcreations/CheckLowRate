dbfeb2ef19450a632832221f3929aeb284860e20
MG Creations <info@mgcreations.co.za>
2020-06-21 19:27:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CheckLowRate
+{
+    class Program
+    {
+        static void Main()
+        {
+            double hourlyRate;
+
+            Console.Write("Enter the hourly rate of pay: ");
+            hourlyRate = Convert.ToDouble(Console.ReadLine());
+
+            if (hourlyRate <= 5.65)
+                Console.WriteLine("Error: Hourly Rate too low.");
+        }
+    }
+}
